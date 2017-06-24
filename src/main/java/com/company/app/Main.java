b8a8915a9d15c6a1eecae4fe9e0ee4ae0adf5075
fedchenko ihor <fedchenko.ihor@gmail.com>
2017-06-24 12:55:25 +0300

    add WHERE condition <, >,...

--- src/main/java/com/company/app/Main.java
+++ src/main/java/com/company/app/Main.java
@@ -18,34 +18,59 @@ public class Main {
 
         MongoClient mongoClient = null;
 
+        mongoClient = new MongoClient(uri);
 
-            mongoClient = new MongoClient(uri);
+        // Get database
+        MongoDatabase database = mongoClient.getDatabase("cars");
 
-            // Get database
-            MongoDatabase database = mongoClient.getDatabase("cars");
+        // Get collection
+        MongoCollection<Document> collection = database.getCollection("sport");
 
-            // Get collection
-            MongoCollection<Document> collection = database.getCollection("sport");
-
-
-            //(SELECT * FROM table)
-            System.out.println("=======");
-            System.out.println("find all entities from collection");
-            System.out.println("=======");
-            collection.find().forEach((Block<Document>) exemple01 -> {
-                System.out.println(exemple01.toJson());
-            });
-
-            //SELECT * FROM table Where field = value
-            System.out.println("=======");
-            System.out.println("find field");
-            System.out.println("=======");
-            collection.find(Filters.eq("brand", "Acura")).forEach((Block<Document>) exemple02 ->{
-            System.out.println(exemple02.toJson());
+        //(SELECT * FROM table)
+        System.out.println("***exemple01***");
+        collection.find().forEach((Block<Document>) exemple01 -> {
+            System.out.println(exemple01.toJson());
         });
 
+        //SELECT * FROM table WHERE field = value
+        System.out.println();
+        System.out.println("***exemple02***");
+        collection.find(Filters.eq("brand", "Acura")).forEach((Block<Document>) exemple02 -> {
+            System.out.println(exemple02.toJson());
+        });
+        //SELECT * FROM table WHERE field > value
+        System.out.println();
+        System.out.println("***exemple03***");
+        collection.find(Filters.gt("rank", 5)).forEach((Block<Document>) exemple03 -> {
+            System.out.println(exemple03.toJson());
+        });
 
+        //SELECT * FROM table WHERE field >= value
+        System.out.println();
+        System.out.println("***exemple04***");
+        collection.find(Filters.gte("rank", 6)).forEach((Block<Document>) exemple04 -> {
+            System.out.println(exemple04.toJson());
+        });
 
-        }
+        //SELECT * FROM table WHERE field < value
+        System.out.println();
+        System.out.println("***exemple05***");
+        collection.find(Filters.lt("rank", 6)).forEach((Block<Document>) exemple05 -> {
+            System.out.println(exemple05.toJson());
+        });
+
+        //SELECT * FROM table WHERE field <= value
+        System.out.println();
+        System.out.println("***exemple06***");
+        collection.find(Filters.lte("rank", 5)).forEach((Block<Document>) exemple06 -> {
+            System.out.println(exemple06.toJson());
+        });
+
+        //SELECT * FROM table WHERE field <> value
+        System.out.println();
+        System.out.println("***exemple07***");
+        collection.find(Filters.ne("rank", 5)).forEach((Block<Document>) exemple07 -> {
+            System.out.println(exemple07.toJson());
+        });
     }
-
+}
